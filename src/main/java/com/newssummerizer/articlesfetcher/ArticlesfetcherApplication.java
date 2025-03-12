3c11cd58c65e77ebfe13698d04aa34870b717ea5
GavrielDabbah <gavriel3@gmail.com>
2025-03-12 13:52:47 +0200

    first commit

---
+++ src/main/java/com/newssummerizer/articlesfetcher/ArticlesfetcherApplication.java
@@ -0,0 +1,13 @@
+package com.newssummerizer.articlesfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesfetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesfetcherApplication.class, args);
+	}
+
+}
